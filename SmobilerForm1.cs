9830afdaa61cef3210b134a4b2431e3c872c8357
Jamcy123 <1219502823@qq.com>
2021-07-21 10:34:35 +0800

    beizhu

---
+++ SmobilerForm1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smobiler.Core;
+using Smobiler.Core.Controls;
+using System.Drawing;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace SmobilerAppTEST7._17
+{
+    partial class SmobilerForm1 : Smobiler.Core.Controls.MobileForm
+    {
+        public SmobilerForm1() : base()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogon_Press(object sender, EventArgs e)
+        {
+            try
+            {
+                string userID = txtUserName.Text.Trim();
+                string PassWord = txtPassword.Text.Trim();
+                if (string.IsNullOrEmpty(userID))
+                    throw new Exception("请输入用户名");
+                if (string.IsNullOrEmpty(PassWord))
+                    throw new Exception("请输入密码");
+                if (checkRemb.Checked == true)
+                {
+                    //记住密码
+
+                    LoadClientData("PWD", PassWord);
+                    //读取密码
+                    ReadClientData("PWD", (object s, ClientDataResultHandlerArgs args) =>
+                     {
+                         if (string.IsNullOrEmpty(args.error))
+                         {
+                             Toast("密码为" + args.Value);
+                         }
+                     });
+                }
+                //连接数据库
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = "server=127.0.0.1;Database=SStudents;uid=root;pwd=;";
+                con.Open();
+                //增加数据并更新同步到数据库
+                string insert = "Insert into Users (ID, Pwd) values("+ txtUserName.Text + ',' + txtPassword.Text + ") ";
+                string delete = "Delete from Users where ID='" + txtUserName.Text + "'";
+                string update= "Update Users set Pwd='" + txtPassword.Text + "' where ID='" + txtUserName.Text + "'";
+                MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(update,con);
+                DataSet dataSet = new DataSet();
+                mySqlDataAdapter.Fill(dataSet);
+                con.Close();
+                //*************************************************
+                //string sql = "select*from Users where ID ='" + txtUserName.Text + "'";//查询语句
+
+                //MySqlDataAdapter find = new MySqlDataAdapter(sql, con);
+                //DataSet save = new DataSet();//缓存
+                //find.Fill(save,"tt");//Fill(DataSet)	在 DataSet 中添加或刷新行。
+
+                //if (save.Tables[0].Rows.Count <= 0)
+                //    throw new Exception("用户不存在，请重新输入！");
+
+                //string pwd = save.Tables[0].Rows[0][1].ToString();
+
+                //if (pwd == txtPassword.Text)
+                //{
+                //    MessageBox.Show("密码正确");
+                //}
+                //else
+                //{
+                //    throw new Exception("密码不正确，请重新输入！");
+                //}
+                //con.Close();
+            }
+            catch (Exception ex)
+            {
+                Toast(ex.Message);
+            }
+        }
+
+        private void btnForget_Press(object sender, EventArgs e)
+        {
+            try
+            {
+                RemoveClientData("PWD", (object s, ClientDataResultHandlerArgs args) =>
+                {
+                    if (string.IsNullOrEmpty(args.error))
+                    {
+                        Toast("密码已成功清除" );
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                Toast(ex.Message);
+            }
+        }
+
+        private void button1_Press(object sender, EventArgs e)
+        {
+            SmobilerForm2 smobilerForm2 = new SmobilerForm2();
+            this.Show(smobilerForm2);
+        }
+    }
+}
---
+++ SmobilerForm2.Designer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Smobiler.Core;
+namespace SmobilerAppTEST7._17
+{
+    partial class SmobilerForm2 : Smobiler.Core.Controls.MobileForm
+    {
+        #region "SmobilerForm generated code "
+
+        //SmobilerForm overrides dispose to clean up the component list.
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+
+        //NOTE: The following procedure is required by the SmobilerForm
+        //It can be modified using the SmobilerForm.  
+        //Do not modify it using the code editor.
+        [System.Diagnostics.DebuggerStepThrough()]
+        private void InitializeComponent()
+        {
+            Smobiler.Core.Controls.IconMenuViewGroup iconMenuViewGroup1 = new Smobiler.Core.Controls.IconMenuViewGroup();
+            Smobiler.Core.Controls.IconMenuViewItem iconMenuViewItem1 = new Smobiler.Core.Controls.IconMenuViewItem();
+            Smobiler.Core.Controls.IconMenuViewItem iconMenuViewItem2 = new Smobiler.Core.Controls.IconMenuViewItem();
+            Smobiler.Core.Controls.IconMenuViewItem iconMenuViewItem3 = new Smobiler.Core.Controls.IconMenuViewItem();
+            Smobiler.Core.Controls.IconMenuViewItem iconMenuViewItem4 = new Smobiler.Core.Controls.IconMenuViewItem();
+            Smobiler.Core.Controls.IconMenuViewItem iconMenuViewItem5 = new Smobiler.Core.Controls.IconMenuViewItem();
+            Smobiler.Core.Controls.IconMenuViewItem iconMenuViewItem6 = new Smobiler.Core.Controls.IconMenuViewItem();
+            this.panel1 = new Smobiler.Core.Controls.Panel();
+            this.iconMenuView1 = new Smobiler.Core.Controls.IconMenuView();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.iconMenuView1});
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(92, 271);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 100);
+            this.panel1.Wrap = Smobiler.Core.Controls.LayoutWrap.Wrap;
+            // 
+            // iconMenuView1
+            // 
+            iconMenuViewGroup1.FontSize = 0F;
+            iconMenuViewGroup1.IconBorderRadius = 50;
+            iconMenuViewGroup1.ItemHeight = 50;
+            iconMenuViewItem1.Icon = "battery-empty";
+            iconMenuViewItem1.ID = "one";
+            iconMenuViewItem1.ImageType = Smobiler.Core.Controls.ImageEx.ImageStyle.FontIcon;
+            iconMenuViewItem1.MessageText = "1";
+            iconMenuViewItem1.Text = "2";
+            iconMenuViewItem1.Value = "3";
+            iconMenuViewItem2.Icon = "battery-quarter";
+            iconMenuViewItem2.ID = "two";
+            iconMenuViewItem2.ImageType = Smobiler.Core.Controls.ImageEx.ImageStyle.FontIcon;
+            iconMenuViewItem2.Text = "2";
+            iconMenuViewItem3.Icon = "battery-half";
+            iconMenuViewItem3.IconPath = "document";
+            iconMenuViewItem3.ID = "three";
+            iconMenuViewItem3.ImageType = Smobiler.Core.Controls.ImageEx.ImageStyle.FontIcon;
+            iconMenuViewItem3.Text = "3";
+            iconMenuViewItem4.Icon = "battery-three-quarters";
+            iconMenuViewItem4.IconPath = "upload";
+            iconMenuViewItem4.ID = "four";
+            iconMenuViewItem4.ImageType = Smobiler.Core.Controls.ImageEx.ImageStyle.FontIcon;
+            iconMenuViewItem4.Text = "4";
+            iconMenuViewItem5.Icon = "battery-full";
+            iconMenuViewItem5.IconPath = "media";
+            iconMenuViewItem5.ID = "five";
+            iconMenuViewItem5.ImageType = Smobiler.Core.Controls.ImageEx.ImageStyle.FontIcon;
+            iconMenuViewItem5.Text = "5";
+            iconMenuViewItem6.Icon = "logon";
+            iconMenuViewItem6.ID = "logon";
+            iconMenuViewItem6.MessageText = "logon";
+            iconMenuViewItem6.Text = "logon1";
+            iconMenuViewGroup1.Items.AddRange(new Smobiler.Core.Controls.IconMenuViewItem[] {
+            iconMenuViewItem1,
+            iconMenuViewItem2,
+            iconMenuViewItem3,
+            iconMenuViewItem4,
+            iconMenuViewItem5,
+            iconMenuViewItem6});
+            this.iconMenuView1.Groups.AddRange(new Smobiler.Core.Controls.IconMenuViewGroup[] {
+            iconMenuViewGroup1});
+            this.iconMenuView1.Location = new System.Drawing.Point(0, 126);
+            this.iconMenuView1.Name = "iconMenuView1";
+            this.iconMenuView1.Size = new System.Drawing.Size(300, 300);
+            // 
+            // SmobilerForm2
+            // 
+            this.BackgroundImage = "white.png";
+            this.BackgroundImageSizeMode = Smobiler.Core.Controls.ImageSizeMode.Stretch;
+            this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.panel1});
+            this.Name = "SmobilerForm2";
+
+        }
+        #endregion
+
+        private Smobiler.Core.Controls.Panel panel1;
+        private Smobiler.Core.Controls.IconMenuView iconMenuView1;
+    }
+}
