a32e077319c796d64a3610b582bea81facebe421
Lingll0 <919728456@qq.com>
2021-08-02 20:37:27 +0800

    .

---
+++ login.Designer.cs
@@ -0,0 +1,158 @@
+﻿using Smobiler.Core;
+
+namespace SmobilerAppTEST7._17
+{
+    partial class SmobilerForm1 : Smobiler.Core.Controls.MobileForm
+    {
+        #region "SmobilerForm Designer generated code "
+
+        //SmobilerForm overrides dispose to clean up the component list.
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+
+        //NOTE: The following procedure is required by the SmobilerForm Designer
+        //It can be modified using the SmobilerForm Designer.  
+        //Do not modify it using the code editor.
+        [System.Diagnostics.DebuggerStepThrough()]
+        private void InitializeComponent()
+        {
+            this.panel1 = new Smobiler.Core.Controls.Panel();
+            this.image1 = new Smobiler.Core.Controls.Image();
+            this.fontIcon1 = new Smobiler.Core.Controls.FontIcon();
+            this.fontIcon2 = new Smobiler.Core.Controls.FontIcon();
+            this.txtUserName = new Smobiler.Core.Controls.TextBox();
+            this.txtPassword = new Smobiler.Core.Controls.TextBox();
+            this.btnLogon = new Smobiler.Core.Controls.Button();
+            this.checkRemb = new Smobiler.Core.Controls.CheckBox();
+            this.label1 = new Smobiler.Core.Controls.Label();
+            this.btnForget = new Smobiler.Core.Controls.Button();
+            this.button1 = new Smobiler.Core.Controls.Button();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.image1,
+            this.fontIcon1,
+            this.fontIcon2,
+            this.txtUserName,
+            this.txtPassword,
+            this.btnLogon,
+            this.checkRemb,
+            this.label1,
+            this.btnForget,
+            this.button1});
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(120, 313);
+            this.panel1.Name = "panel1";
+            this.panel1.Scrollable = true;
+            this.panel1.Size = new System.Drawing.Size(300, 100);
+            // 
+            // image1
+            // 
+            this.image1.Location = new System.Drawing.Point(98, 57);
+            this.image1.Name = "image1";
+            this.image1.ResourceID = "sansan1";
+            this.image1.Size = new System.Drawing.Size(122, 80);
+            // 
+            // fontIcon1
+            // 
+            this.fontIcon1.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.fontIcon1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.fontIcon1.Location = new System.Drawing.Point(31, 239);
+            this.fontIcon1.Name = "fontIcon1";
+            this.fontIcon1.ResourceID = "user";
+            this.fontIcon1.Size = new System.Drawing.Size(25, 25);
+            // 
+            // fontIcon2
+            // 
+            this.fontIcon2.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.fontIcon2.Location = new System.Drawing.Point(31, 296);
+            this.fontIcon2.Name = "fontIcon2";
+            this.fontIcon2.ResourceID = "lock";
+            this.fontIcon2.Size = new System.Drawing.Size(25, 25);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(72, 235);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(200, 30);
+            this.txtUserName.WaterMarkText = "账号";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(72, 293);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.SecurityMode = true;
+            this.txtPassword.Size = new System.Drawing.Size(200, 30);
+            this.txtPassword.WaterMarkText = "密码";
+            // 
+            // btnLogon
+            // 
+            this.btnLogon.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnLogon.FontSize = 15F;
+            this.btnLogon.Location = new System.Drawing.Point(71, 378);
+            this.btnLogon.Name = "btnLogon";
+            this.btnLogon.Size = new System.Drawing.Size(170, 35);
+            this.btnLogon.Text = "登录";
+            this.btnLogon.Press += new System.EventHandler(this.btnLogon_Press);
+            // 
+            // checkRemb
+            // 
+            this.checkRemb.Location = new System.Drawing.Point(35, 347);
+            this.checkRemb.Name = "checkRemb";
+            this.checkRemb.Size = new System.Drawing.Size(15, 15);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(72, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.Text = "记住密码";
+            // 
+            // btnForget
+            // 
+            this.btnForget.BackColor = System.Drawing.Color.White;
+            this.btnForget.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnForget.Location = new System.Drawing.Point(180, 343);
+            this.btnForget.Name = "btnForget";
+            this.btnForget.Size = new System.Drawing.Size(80, 25);
+            this.btnForget.Text = "清除密码";
+            this.btnForget.Press += new System.EventHandler(this.btnForget_Press);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.Text = "button1";
+            this.button1.Press += new System.EventHandler(this.button1_Press);
+            // 
+            // SmobilerForm1
+            // 
+            this.BackgroundImage = "bluee";
+            this.BackgroundImageSizeMode = Smobiler.Core.Controls.ImageSizeMode.Stretch;
+            this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.panel1});
+            this.Name = "SmobilerForm1";
+            this.Name = "SmobilerForm1";
+
+        }
+        #endregion
+
+        private Smobiler.Core.Controls.Panel panel1;
+        private Smobiler.Core.Controls.Image image1;
+        private Smobiler.Core.Controls.FontIcon fontIcon1;
+        private Smobiler.Core.Controls.FontIcon fontIcon2;
+        private Smobiler.Core.Controls.TextBox txtUserName;
+        private Smobiler.Core.Controls.TextBox txtPassword;
+        private Smobiler.Core.Controls.Button btnLogon;
+        private Smobiler.Core.Controls.CheckBox checkRemb;
+        private Smobiler.Core.Controls.Label label1;
+        private Smobiler.Core.Controls.Button btnForget;
+        private Smobiler.Core.Controls.Button button1;
+    }
+}
