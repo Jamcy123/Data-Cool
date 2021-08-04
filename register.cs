cc305381d62378901079200efeeaa67c4c36fa5f
Lingll0 <919728456@qq.com>
2021-08-04 17:44:40 +0800

    17:44

--- register.cs
+++ register.cs
@@ -29,6 +29,8 @@ namespace SmobilerAppTEST7._17
             MySqlCommand da = new MySqlCommand(judge, con);
             MySqlDataReader dr = da.ExecuteReader();
             bool b = dr.Read();
+            con.Close();
+            con.Open();
             if (b)
             {
                 Toast("该电话号码已注册");
@@ -49,22 +51,29 @@ namespace SmobilerAppTEST7._17
                         txtsure.Text = "";
                         Toast("密码错误，请重新输入");
                     }
-                    if (checkBox1.Checked == true)
-                        sex = "男";
                     else
-                        sex = "女";
-                    string insert = "Insert into Userinf (Uphoneno,Upassword,Ublance,Usex) values(\"" + txtUserphone.Text + "\",\"" + txtPassword.Text + "\",\"" + "0" + "\",\"" + sex + "\")";
-                    MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(insert, con);
-                    DataSet dataSet = new DataSet();
-                    mySqlDataAdapter.Fill(dataSet);
-                    string select = "Select Upassword from Userinf where Uphoneno=" + txtUserphone.Text;
-                    MySqlDataAdapter find = new MySqlDataAdapter(select, con);
-                    DataSet save = new DataSet();//缓存
-                    find.Fill(save);//Fill(DataSet)	在 DataSet 中添加或刷新行。
-                    if (save.Tables[0].Rows.Count <= 0)
-                        Toast("注册失败！");
-                    else
-                        Toast("注册成功！");
+                    {
+                        if (checkBox1.Checked == true)
+                            sex = "男";
+                        else
+                            sex = "女";
+                        string insert = "Insert into Userinf (Uphoneno,Upassword,Ublance,Usex) values(\"" + txtUserphone.Text + "\",\"" + txtPassword.Text + "\",\"" + "0" + "\",\"" + sex + "\")";
+                        MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(insert, con);
+                        DataSet dataSet = new DataSet();
+                        mySqlDataAdapter.Fill(dataSet);
+                        string select = "Select Upassword from Userinf where Uphoneno=" + txtUserphone.Text;
+                        MySqlDataAdapter find = new MySqlDataAdapter(select, con);
+                        DataSet save = new DataSet();//缓存
+                        find.Fill(save);//Fill(DataSet)	在 DataSet 中添加或刷新行。
+                        if (save.Tables[0].Rows.Count <= 0)
+                            Toast("注册失败！");
+                        else
+                        {
+                            Toast("注册成功！");
+                            this.Close();
+                        }
+                    }
+                    
                 }
             }
             con.Close();
