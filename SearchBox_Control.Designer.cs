46a7f6bd8a34d1cab87aff2725412744e1c0eab7
Jamcy123 <1219502823@qq.com>
2021-08-06 18:02:03 +0800

    18：01

---
+++ SearchBox_Control.Designer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Smobiler.Core;
+namespace SmobilerAppTEST7._17
+{
+    partial class SearchBox_Control : Smobiler.Core.Controls.MobileUserControl
+    {
+        #region "SmobilerUserControl generated code "
+
+        //SmobilerUserControl overrides dispose to clean up the component list.
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        //NOTE: The following procedure is required by the SmobilerUserControl
+        //It can be modified using the SmobilerUserControl.  
+        //Do not modify it using the code editor.
+        [System.Diagnostics.DebuggerStepThrough()]
+        private void InitializeComponent()
+        {
+            this.panel1 = new Smobiler.Core.Controls.Panel();
+            this.textBox1 = new Smobiler.Core.Controls.TextBox();
+            this.panel2 = new Smobiler.Core.Controls.Panel();
+            this.fontIcon1 = new Smobiler.Core.Controls.FontIcon();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.BorderRadius = 40;
+            this.panel1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.textBox1,
+            this.panel2});
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 40);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 28);
+            this.textBox1.WaterMarkText = "输入查询内容";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.fontIcon1});
+            this.panel2.Location = new System.Drawing.Point(254, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(28, 28);
+            // 
+            // fontIcon1
+            // 
+            this.fontIcon1.BorderColor = System.Drawing.Color.Transparent;
+            this.fontIcon1.ForeColor = System.Drawing.Color.DimGray;
+            this.fontIcon1.Location = new System.Drawing.Point(3, 3);
+            this.fontIcon1.Name = "fontIcon1";
+            this.fontIcon1.ResourceID = "search";
+            this.fontIcon1.Size = new System.Drawing.Size(23, 23);
+            // 
+            // SearchBox_Control
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.panel1});
+            this.Layout = Smobiler.Core.Controls.LayoutPosition.Relative;
+            this.Size = new System.Drawing.Size(300, 40);
+            this.Name = "SearchBox_Control";
+
+        }
+        #endregion
+
+        private Smobiler.Core.Controls.Panel panel1;
+        private Smobiler.Core.Controls.TextBox textBox1;
+        private Smobiler.Core.Controls.Panel panel2;
+        private Smobiler.Core.Controls.FontIcon fontIcon1;
+    }
+}
--- Search_Form.Designer.cs
+++ Search_Form.Designer.cs
@@ -19,8 +19,26 @@ namespace SmobilerAppTEST7._17
         [System.Diagnostics.DebuggerStepThrough()]
         private void InitializeComponent()
         {
-            this.title_Control1 = new SmobilerAppTEST7._17.Title_Control();
             this.panel1 = new Smobiler.Core.Controls.Panel();
+            this.title_Control1 = new SmobilerAppTEST7._17.Title_Control();
+            this.searchBox_Control1 = new SmobilerAppTEST7._17.SearchBox_Control();
+            this.panel2 = new Smobiler.Core.Controls.Panel();
+            this.checkBox2 = new Smobiler.Core.Controls.CheckBox();
+            this.checkBox1 = new Smobiler.Core.Controls.CheckBox();
+            this.listView1 = new Smobiler.Core.Controls.ListView();
+            this.label1 = new Smobiler.Core.Controls.Label();
+            this.label2 = new Smobiler.Core.Controls.Label();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.searchBox_Control1,
+            this.panel2,
+            this.listView1});
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(149, 246);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 100);
             // 
             // title_Control1
             // 
@@ -31,15 +49,62 @@ namespace SmobilerAppTEST7._17
             this.title_Control1.Location = new System.Drawing.Point(53, 59);
             this.title_Control1.Name = "title_Control1";
             this.title_Control1.Size = new System.Drawing.Size(100, 36);
-            this.title_Control1.Text = "标题";
+            this.title_Control1.Text = "搜索";
             this.title_Control1.ExitButtonpPress += new System.EventHandler(this.title_Control1_ExitButtonpPress_1);
             // 
-            // panel1
+            // searchBox_Control1
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(149, 246);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(300, 100);
+            this.searchBox_Control1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.searchBox_Control1.Layout = Smobiler.Core.Controls.LayoutPosition.Relative;
+            this.searchBox_Control1.Location = new System.Drawing.Point(0, 103);
+            this.searchBox_Control1.Name = "searchBox_Control1";
+            this.searchBox_Control1.Size = new System.Drawing.Size(300, 40);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.checkBox2,
+            this.checkBox1,
+            this.label1,
+            this.label2});
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 75);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 42);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Location = new System.Drawing.Point(170, 8);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(22, 22);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(30, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 22);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(23, 208);
+            this.listView1.Name = "listView1";
+            this.listView1.PageSizeTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(145)))), ((int)(((byte)(145)))));
+            this.listView1.Size = new System.Drawing.Size(300, 300);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(54, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 22);
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(195, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 22);
+            this.label2.Text = "label1";
             // 
             // Search_Form
             // 
@@ -54,5 +119,12 @@ namespace SmobilerAppTEST7._17
 
         private Title_Control title_Control1;
         private Smobiler.Core.Controls.Panel panel1;
+        private SearchBox_Control searchBox_Control1;
+        private Smobiler.Core.Controls.Panel panel2;
+        private Smobiler.Core.Controls.CheckBox checkBox2;
+        private Smobiler.Core.Controls.CheckBox checkBox1;
+        private Smobiler.Core.Controls.ListView listView1;
+        private Smobiler.Core.Controls.Label label1;
+        private Smobiler.Core.Controls.Label label2;
     }
 }
