d80336eff7c121f0d7cbb1060b8c030cb505e135
Jamcy123 <1219502823@qq.com>
2021-08-07 15:35:06 +0800

    15:35

--- Cinema_Control.Designer.cs
+++ Cinema_Control.Designer.cs
@@ -19,8 +19,8 @@ namespace SmobilerAppTEST7._17
         private void InitializeComponent()
         {
             this.panel1 = new Smobiler.Core.Controls.Panel();
-            this.label1 = new Smobiler.Core.Controls.Label();
-            this.label4 = new Smobiler.Core.Controls.Label();
+            this.Cname_lbl = new Smobiler.Core.Controls.Label();
+            this.Caddress_lbl = new Smobiler.Core.Controls.Label();
             // 
             // panel1
             // 
@@ -28,45 +28,49 @@ namespace SmobilerAppTEST7._17
             this.panel1.Border = new Smobiler.Core.Controls.Border(0F, 0F, 0F, 1F);
             this.panel1.BorderColor = System.Drawing.Color.LightGray;
             this.panel1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
-            this.label1,
-            this.label4});
+            this.Cname_lbl,
+            this.Caddress_lbl});
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(111, 312);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(300, 100);
             this.panel1.Touchable = true;
+            this.panel1.Press += new System.EventHandler(this.panel1_Press);
             // 
-            // label1
+            // Cname_lbl
             // 
-            this.label1.Bold = true;
-            this.label1.DataMember = "Cno";
-            this.label1.DisplayMember = "Cname";
-            this.label1.FontSize = 15F;
-            this.label1.Location = new System.Drawing.Point(14, 11);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 26);
-            this.label1.Text = "label1";
+            this.Cname_lbl.Bold = true;
+            this.Cname_lbl.DataMember = "Cno";
+            this.Cname_lbl.DisplayMember = "Cname";
+            this.Cname_lbl.FontSize = 15F;
+            this.Cname_lbl.Location = new System.Drawing.Point(14, 7);
+            this.Cname_lbl.Name = "Cname_lbl";
+            this.Cname_lbl.Selectable = true;
+            this.Cname_lbl.Size = new System.Drawing.Size(193, 26);
+            this.Cname_lbl.Text = "Cname_lbl";
             // 
-            // label4
+            // Caddress_lbl
             // 
-            this.label4.DisplayMember = "Caddress";
-            this.label4.Location = new System.Drawing.Point(14, 47);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(274, 23);
-            this.label4.Text = "label4";
+            this.Caddress_lbl.DisplayMember = "Caddress";
+            this.Caddress_lbl.ForeColor = System.Drawing.Color.DarkGray;
+            this.Caddress_lbl.Location = new System.Drawing.Point(14, 39);
+            this.Caddress_lbl.Name = "Caddress_lbl";
+            this.Caddress_lbl.Selectable = true;
+            this.Caddress_lbl.Size = new System.Drawing.Size(238, 23);
+            this.Caddress_lbl.Text = "Caddress_lbl";
             // 
             // Cinema_Control
             // 
             this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
             this.panel1});
-            this.Size = new System.Drawing.Size(300, 100);
+            this.Size = new System.Drawing.Size(300, 70);
             this.Name = "Cinema_Control";
 
         }
         #endregion
 
         private Smobiler.Core.Controls.Panel panel1;
-        private Smobiler.Core.Controls.Label label1;
-        private Smobiler.Core.Controls.Label label4;
+        private Smobiler.Core.Controls.Label Cname_lbl;
+        private Smobiler.Core.Controls.Label Caddress_lbl;
     }
 }
--- Search_Form.cs
+++ Search_Form.cs
@@ -42,14 +42,17 @@ namespace SmobilerAppTEST7._17
                 string database = "Movie_ticket";
                 string sql = "";
                 DataSet dataSet = new DataSet();
+                string BoxText = searchBox_Control1.TextBox_Text.Replace("'", "''");//使数据库能正确识别字符串
                 switch (tabPageView1.PageIndex)
                 {
                     case 0://查询电影
-                        sql = "select * from Movie_ticket.Movie where Mname like '%" + searchBox_Control1.TextBox_Text + "%'";
+                        sql = "select * from Movie_ticket.Movie where Mname like '%" + BoxText + "%'";
                         dataSet = Databaseconnect(database, sql);
                         if (dataSet.Tables[0].Rows.Count <= 0)
                         {
-                            Toast("此处暂无符合要求的电影");
+                            listView1.DataSource = dataSet;
+                            listView1.DataBind();
+                            throw new Exception("此处暂无符合要求的电影");
                         }
                         else
                         {
@@ -58,12 +61,13 @@ namespace SmobilerAppTEST7._17
                         }
                         break;
                     case 1://查询用户所在地的影院
-                        sql = "select Cno,Cname,Caddress from Cinema,Userinf where Ucity=Ccity and Cname like '%" + searchBox_Control1.TextBox_Text + "%' and Uphoneno='" + Uno + "'";
-                        searchBox_Control1.TextBox_Text = sql;
+                        sql = "select Cno,Cname,Caddress from Cinema,Userinf where Ucity=Ccity and Cname like '%" + BoxText + "%' and Uphoneno='" + Uno + "'";
                         dataSet = Databaseconnect(database, sql);
                         if (dataSet.Tables[0].Rows.Count <= 0)
                         {
-                            Toast("此处暂无符合要求的影院");
+                            listView2.DataSource = dataSet;
+                            listView2.DataBind();
+                            throw new Exception("此处暂无符合要求的电影");
                         }
                         else
                         {
