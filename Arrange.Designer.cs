aa02ef2aa5cc5c378f6db4b5949cefa34b6c6318
Lingll0 <919728456@qq.com>
2021-08-13 20:17:30 +0800

    .

--- Arrange.Designer.cs
+++ Arrange.Designer.cs
@@ -32,6 +32,16 @@ namespace SmobilerAppTEST7._17
             this.label2 = new Smobiler.Core.Controls.Label();
             this.label3 = new Smobiler.Core.Controls.Label();
             this.Mduration_lbl = new Smobiler.Core.Controls.Label();
+            this.tabPageView1 = new Smobiler.Core.Controls.TabPageView();
+            this.tabPageControl1 = new Smobiler.Core.Controls.TabPageControl();
+            this.listView1 = new Smobiler.Core.Controls.ListView();
+            this.tabPageControl2 = new Smobiler.Core.Controls.TabPageControl();
+            this.listView2 = new Smobiler.Core.Controls.ListView();
+            this.tabPageControl3 = new Smobiler.Core.Controls.TabPageControl();
+            this.listView3 = new Smobiler.Core.Controls.ListView();
+            this.button1 = new Smobiler.Core.Controls.Button();
+            this.button2 = new Smobiler.Core.Controls.Button();
+            this.button3 = new Smobiler.Core.Controls.Button();
             // 
             // title_Control1
             // 
@@ -63,12 +73,12 @@ namespace SmobilerAppTEST7._17
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 36);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(300, 164);
+            this.panel1.Size = new System.Drawing.Size(300, 152);
             // 
             // label4
             // 
             this.label4.ForeColor = System.Drawing.Color.Gray;
-            this.label4.Location = new System.Drawing.Point(136, 133);
+            this.label4.Location = new System.Drawing.Point(137, 129);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 17);
             this.label4.Text = "电影时长";
@@ -76,7 +86,7 @@ namespace SmobilerAppTEST7._17
             // label1
             // 
             this.label1.ForeColor = System.Drawing.Color.Gray;
-            this.label1.Location = new System.Drawing.Point(136, 108);
+            this.label1.Location = new System.Drawing.Point(137, 104);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 17);
             this.label1.Text = "电影语言";
@@ -86,7 +96,7 @@ namespace SmobilerAppTEST7._17
             this.Mgrade_lbl.DisplayMember = "Mgrade";
             this.Mgrade_lbl.FontSize = 14F;
             this.Mgrade_lbl.ForeColor = System.Drawing.Color.DarkOrange;
-            this.Mgrade_lbl.Location = new System.Drawing.Point(208, 58);
+            this.Mgrade_lbl.Location = new System.Drawing.Point(209, 54);
             this.Mgrade_lbl.Name = "Mgrade_lbl";
             this.Mgrade_lbl.Size = new System.Drawing.Size(50, 17);
             this.Mgrade_lbl.Text = "label4";
@@ -94,7 +104,7 @@ namespace SmobilerAppTEST7._17
             // Mtype_lbl
             // 
             this.Mtype_lbl.DisplayMember = "Mtype";
-            this.Mtype_lbl.Location = new System.Drawing.Point(208, 83);
+            this.Mtype_lbl.Location = new System.Drawing.Point(209, 79);
             this.Mtype_lbl.Name = "Mtype_lbl";
             this.Mtype_lbl.Size = new System.Drawing.Size(76, 17);
             this.Mtype_lbl.Text = "label5";
@@ -102,7 +112,7 @@ namespace SmobilerAppTEST7._17
             // Mlanguage_lbl
             // 
             this.Mlanguage_lbl.DisplayMember = "Mlanguage";
-            this.Mlanguage_lbl.Location = new System.Drawing.Point(208, 108);
+            this.Mlanguage_lbl.Location = new System.Drawing.Point(209, 104);
             this.Mlanguage_lbl.Name = "Mlanguage_lbl";
             this.Mlanguage_lbl.Size = new System.Drawing.Size(76, 17);
             this.Mlanguage_lbl.Text = "label5";
@@ -111,7 +121,7 @@ namespace SmobilerAppTEST7._17
             // 
             this.Mpicadress_image.BorderRadius = 7;
             this.Mpicadress_image.DisplayMember = "Mpicadress";
-            this.Mpicadress_image.Location = new System.Drawing.Point(12, 16);
+            this.Mpicadress_image.Location = new System.Drawing.Point(13, 12);
             this.Mpicadress_image.Name = "Mpicadress_image";
             this.Mpicadress_image.ResourceID = global::SmobilerAppTEST7._17.Properties.Settings.Default.Mpicadress;
             this.Mpicadress_image.Size = new System.Drawing.Size(100, 134);
@@ -122,7 +132,7 @@ namespace SmobilerAppTEST7._17
             this.Mname_lbl.DataMember = "Mno";
             this.Mname_lbl.DisplayMember = "Mname";
             this.Mname_lbl.FontSize = 17F;
-            this.Mname_lbl.Location = new System.Drawing.Point(136, 16);
+            this.Mname_lbl.Location = new System.Drawing.Point(137, 12);
             this.Mname_lbl.Name = "Mname_lbl";
             this.Mname_lbl.Size = new System.Drawing.Size(148, 26);
             this.Mname_lbl.Text = "label1";
@@ -130,7 +140,7 @@ namespace SmobilerAppTEST7._17
             // label2
             // 
             this.label2.ForeColor = System.Drawing.Color.Gray;
-            this.label2.Location = new System.Drawing.Point(136, 58);
+            this.label2.Location = new System.Drawing.Point(137, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 17);
             this.label2.Text = "电影评分";
@@ -138,7 +148,7 @@ namespace SmobilerAppTEST7._17
             // label3
             // 
             this.label3.ForeColor = System.Drawing.Color.Gray;
-            this.label3.Location = new System.Drawing.Point(136, 83);
+            this.label3.Location = new System.Drawing.Point(137, 79);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 17);
             this.label3.Text = "电影类型";
@@ -148,16 +158,100 @@ namespace SmobilerAppTEST7._17
             this.Mduration_lbl.DisplayMember = "Mgrade";
             this.Mduration_lbl.FontSize = 14F;
             this.Mduration_lbl.ForeColor = System.Drawing.Color.DarkOrange;
-            this.Mduration_lbl.Location = new System.Drawing.Point(208, 133);
+            this.Mduration_lbl.Location = new System.Drawing.Point(209, 129);
             this.Mduration_lbl.Name = "Mduration_lbl";
             this.Mduration_lbl.Size = new System.Drawing.Size(50, 17);
             this.Mduration_lbl.Text = "label4";
+            // 
+            // tabPageView1
+            // 
+            this.tabPageView1.BackColor = System.Drawing.Color.White;
+            this.tabPageView1.Border = new Smobiler.Core.Controls.Border(0F, 1F, 0F, 1F);
+            this.tabPageView1.BorderColor = System.Drawing.Color.LightGray;
+            this.tabPageView1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.tabPageControl1,
+            this.tabPageControl2,
+            this.tabPageControl3});
+            this.tabPageView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabPageView1.Location = new System.Drawing.Point(0, 244);
+            this.tabPageView1.Name = "tabPageView1";
+            this.tabPageView1.Size = new System.Drawing.Size(300, 312);
+            // 
+            // tabPageControl1
+            // 
+            this.tabPageControl1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.listView1,
+            this.button1});
+            this.tabPageControl1.Name = "tabPageControl1";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 31);
+            this.listView1.Name = "listView1";
+            this.listView1.PageSizeTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(145)))), ((int)(((byte)(145)))));
+            this.listView1.Size = new System.Drawing.Size(300, 203);
+            this.listView1.TemplateControlName = "Session";
+            // 
+            // tabPageControl2
+            // 
+            this.tabPageControl2.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.listView2,
+            this.button2});
+            this.tabPageControl2.Name = "tabPageControl2";
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(0, 31);
+            this.listView2.Name = "listView2";
+            this.listView2.PageSizeTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(145)))), ((int)(((byte)(145)))));
+            this.listView2.Size = new System.Drawing.Size(300, 203);
+            this.listView2.TemplateControlName = "Session";
+            // 
+            // tabPageControl3
+            // 
+            this.tabPageControl3.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.listView3,
+            this.button3});
+            this.tabPageControl3.Name = "tabPageControl3";
+            // 
+            // listView3
+            // 
+            this.listView3.Location = new System.Drawing.Point(0, 31);
+            this.listView3.Name = "listView3";
+            this.listView3.PageSizeTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(145)))), ((int)(((byte)(145)))));
+            this.listView3.Size = new System.Drawing.Size(300, 203);
+            this.listView3.TemplateControlName = "Session";
+            // 
+            // button1
+            // 
+            this.button1.FontSize = 16F;
+            this.button1.Location = new System.Drawing.Point(97, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 40);
+            this.button1.Text = "添加场次";
+            // 
+            // button2
+            // 
+            this.button2.FontSize = 16F;
+            this.button2.Location = new System.Drawing.Point(104, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.Text = "添加场次";
+            // 
+            // button3
+            // 
+            this.button3.FontSize = 16F;
+            this.button3.Location = new System.Drawing.Point(104, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 40);
+            this.button3.Text = "添加场次";
             // 
             // Arrange
             // 
             this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
             this.title_Control1,
-            this.panel1});
+            this.panel1,
+            this.tabPageView1});
             this.Name = "Arrange";
 
         }
@@ -175,5 +269,15 @@ namespace SmobilerAppTEST7._17
         private Smobiler.Core.Controls.Label label2;
         private Smobiler.Core.Controls.Label label3;
         private Smobiler.Core.Controls.Label Mduration_lbl;
+        private Smobiler.Core.Controls.TabPageView tabPageView1;
+        private Smobiler.Core.Controls.TabPageControl tabPageControl1;
+        private Smobiler.Core.Controls.ListView listView1;
+        private Smobiler.Core.Controls.Button button1;
+        private Smobiler.Core.Controls.TabPageControl tabPageControl2;
+        private Smobiler.Core.Controls.ListView listView2;
+        private Smobiler.Core.Controls.TabPageControl tabPageControl3;
+        private Smobiler.Core.Controls.ListView listView3;
+        private Smobiler.Core.Controls.Button button2;
+        private Smobiler.Core.Controls.Button button3;
     }
 }
---
+++ Session.Designer.cs
@@ -0,0 +1,111 @@
+﻿using Smobiler.Core;
+using System;
+
+namespace SmobilerAppTEST7._17
+{
+    partial class Session : Smobiler.Core.Controls.MobileUserControl
+    {
+        #region "SmobilerUserControl generated code "
+
+        //SmobilerUserControl overrides dispose to clean up the component list.
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        //NOTE: The following procedure is required by the SmobilerUserControl
+        //It can be modified using the SmobilerUserControl.  
+        //Do not modify it using the code editor.
+        [System.Diagnostics.DebuggerStepThrough()]
+        private void InitializeComponent()
+        {
+            this.panel1 = new Smobiler.Core.Controls.Panel();
+            this.label1 = new Smobiler.Core.Controls.Label();
+            this.label2 = new Smobiler.Core.Controls.Label();
+            this.label3 = new Smobiler.Core.Controls.Label();
+            this.label4 = new Smobiler.Core.Controls.Label();
+            this.label5 = new Smobiler.Core.Controls.Label();
+            this.button1 = new Smobiler.Core.Controls.Button();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.label1,
+            this.label2,
+            this.label3,
+            this.label4,
+            this.label5,
+            this.button1});
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(136, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 100);
+            // 
+            // label1
+            // 
+            this.label1.FontSize = 16F;
+            this.label1.Location = new System.Drawing.Point(0, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 35);
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(75, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 35);
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.FontSize = 10F;
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(75, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 23);
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.FontSize = 16F;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(164, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 41);
+            this.label4.Text = "￥31.9";
+            // 
+            // label5
+            // 
+            this.label5.FontSize = 10F;
+            this.label5.ForeColor = System.Drawing.Color.DimGray;
+            this.label5.Location = new System.Drawing.Point(0, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 23);
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 34);
+            this.button1.Text = "删除";
+            // 
+            // Session
+            // 
+            this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.panel1});
+            this.Size = new System.Drawing.Size(300, 100);
+            this.Name = "Session";
+
+        }
+        #endregion
+
+        private Smobiler.Core.Controls.Panel panel1;
+        private Smobiler.Core.Controls.Label label1;
+        private Smobiler.Core.Controls.Label label2;
+        private Smobiler.Core.Controls.Label label3;
+        private Smobiler.Core.Controls.Label label4;
+        private Smobiler.Core.Controls.Label label5;
+        private Smobiler.Core.Controls.Button button1;
+    }
+}
